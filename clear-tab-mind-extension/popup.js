5cf2b5a7894f129ec3535fd25006164664df4e5f
Blitz00X <143121150+Blitz00X@users.noreply.github.com>
2025-08-07 15:40:31 +0300

    Update popup.js

--- clear-tab-mind-extension/popup.js
+++ clear-tab-mind-extension/popup.js
@@ -171,11 +171,11 @@ class ClearTabMindPopup {
     }
   }
 
-  openCollection(collection) {
-    this.currentCollection = collection;
-    this.view = 'tabs';
-    this.loadTabsFromCollection(collection.id);
-    this.render();
+  async openCollection(collection) {
+      this.currentCollection = collection;
+      this.view = 'tabs';
+      await this.loadTabsFromCollection(collection.id);
+      this.render();
   }
 
   backToCollections() {
